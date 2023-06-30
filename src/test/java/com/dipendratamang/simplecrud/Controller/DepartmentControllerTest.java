5fba8cc15f8f9620f89b376929241d05c8ecc343
dipendra <dipendra.tamang9@gmail.com>
2023-06-30 10:26:07 +0545

    terst case for controller

---
+++ src/test/java/com/dipendratamang/simplecrud/Controller/DepartmentControllerTest.java
@@ -0,0 +1,62 @@
+package com.dipendratamang.simplecrud.Controller;
+
+import com.dipendratamang.simplecrud.entity.Department;
+import com.dipendratamang.simplecrud.service.DepartmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DepartmentService departmentService;
+
+    private Department department;
+    @BeforeEach
+    void setUp(){
+        department= Department.builder()
+                .departmentAddress("Jhapa")
+                .departmentCode("IT-2080")
+                .departmentName("IT")
+                .departmentId(1L)
+                .build();
+
+    }
+    @Test
+    void saveDepartment() throws Exception {
+       Department inputDepartment= Department.builder()
+                .departmentAddress("Jhapa")
+                .departmentCode("IT-06")
+                .departmentName("IT")
+                .departmentId(1L)
+                .build();
+        Mockito.when(departmentService.saveDepartment(inputDepartment)).thenReturn(department);
+        mockMvc.perform(post("/departments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"departmentName\":\"IT\",\n" +
+                        "    \"departmentAddress\":\"Jhapa\",\n" +
+                        "    \"departmentCode\":\"IT-2080\"\n" +
+                        "}"))
+                .andExpect(status().isOk());
+
+
+    }
+
+    @Test
+    void fetchDepartmentById() {
+    }
+}
--- src/test/java/com/dipendratamang/simplecrud/service/DepartmentServiceTest.java
+++ src/test/java/com/dipendratamang/simplecrud/service/DepartmentServiceTest.java
@@ -3,13 +3,16 @@ package com.dipendratamang.simplecrud.service;
 import com.dipendratamang.simplecrud.entity.Department;
 import com.dipendratamang.simplecrud.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class DepartmentServiceTest {
     @MockBean
     private DepartmentRepository departmentRepository;
@@ -22,12 +25,14 @@ class DepartmentServiceTest {
                         .departmentName("IT")
                         .departmentAddress("Jhapa")
                         .departmentCode("IT-06")
+                        .departmentId(1L)
                         .build();
         Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT")).thenReturn(department);
 
     }
 
     @Test
+    @DisplayName("Get Data based on valid Department Name")
     public void whenValidDepartmentName_thenDepartmentShouldFound(){
         String departmentName="IT";
         Department found = departmentService.fetchDepartmentByName(departmentName);
